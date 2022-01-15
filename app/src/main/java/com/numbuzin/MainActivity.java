790a7454cbe19e529bd2907e3246387c0f0b4deb
jun0925 <yeongjun0925@gmail.com>
2022-01-15 09:49:58 +0900

    webview

--- app/src/main/java/com/numbuzin/MainActivity.java
+++ app/src/main/java/com/numbuzin/MainActivity.java
@@ -1,14 +1,99 @@
 package com.numbuzin;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private long backKeyPressedTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Alert")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok,
+                                new AlertDialog.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        result.confirm();
+                                    }
+                                })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+                return true;
+            }
+
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message,
+                                       final JsResult result) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Confirm")
+                        .setMessage(message)
+                        .setPositiveButton("Yes",
+                                new AlertDialog.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        result.confirm();
+                                    }
+                                })
+                        .setNegativeButton("No",
+                                new AlertDialog.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        result.cancel();
+                                    }
+                                })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+                return true;
+            }
+        });
+
+        mWebView.loadUrl("https://m.numbuzin.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        }else{
+            if(System.currentTimeMillis()-backKeyPressedTime>=2000){
+                backKeyPressedTime=System.currentTimeMillis();
+                Toast.makeText(getApplicationContext(),"'뒤로'버튼 한번 더 누르시면 종료됩니다.",Toast.LENGTH_SHORT).show();
+            }else if(System.currentTimeMillis()-backKeyPressedTime<2000){
+                finish();
+                return;
+            }
+        }
     }
 }
